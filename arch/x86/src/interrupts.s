b55ac0c64c8f105f85c0b6e7556efccd52088648
saliccio <fsalik25@outlook.com>
2025-05-11 23:21:06 +0300

    delete explicit byte notation

--- arch/x86/src/interrupts.s
+++ arch/x86/src/interrupts.s
@@ -143,313 +143,313 @@ global _irq15
 ; 0: Divide By Zero Exception
 _exc0:
     cli  ; The very first thing to do when an ISR runs is to clear interrupt flag (disable maskable interrupts)
-    push byte 0  ; Push error code (some exceptions already do that)
-    push byte 0  ; Push interrupt number
+    push 0  ; Push error code (some exceptions already do that)
+    push 0  ; Push interrupt number
     jmp exc_housekeeping  ; Jump to the housekeeping code
 
 ; 1: Debug Exception
 _exc1:
     cli
-    push byte 0
-    push byte 1
+    push 0
+    push 1
     jmp exc_housekeeping
 
 ; 2: Non Maskable Interrupt Exception
 _exc2:
     cli
-    push byte 0
-    push byte 2
+    push 0
+    push 2
     jmp exc_housekeeping
 
 ; 3: Int 3 Exception
 _exc3:
     cli
-    push byte 0
-    push byte 3
+    push 0
+    push 3
     jmp exc_housekeeping
 
 ; 4: INTO Exception
 _exc4:
     cli
-    push byte 0
-    push byte 4
+    push 0
+    push 4
     jmp exc_housekeeping
 
 ; 5: Out of Bounds Exception
 _exc5:
     cli
-    push byte 0
-    push byte 5
+    push 0
+    push 5
     jmp exc_housekeeping
 
 ; 6: Invalid Opcode Exception
 _exc6:
     cli
-    push byte 0
-    push byte 6
+    push 0
+    push 6
     jmp exc_housekeeping
 
 ; 7: Coprocessor Not Available Exception
 _exc7:
     cli
-    push byte 0
-    push byte 7
+    push 0
+    push 7
     jmp exc_housekeeping
 
 ; 8: Double Fault Exception (With Error Code)
 _exc8:
     cli
-    push byte 8
+    push 8
     jmp exc_housekeeping
 
 ; 9: Coprocessor Segment Overrun Exception
 _exc9:
     cli
-    push byte 0
-    push byte 9
+    push 0
+    push 9
     jmp exc_housekeeping
 
 ; 10: Bad TSS Exception (With Error Code)
 _exc10:
     cli
-    push byte 10
+    push 10
     jmp exc_housekeeping
 
 ; 11: Segment Not Present Exception (With Error Code)
 _exc11:
     cli
-    push byte 11
+    push 11
     jmp exc_housekeeping
 
 ; 12: Stack Fault Exception (With Error Code)
 _exc12:
     cli
-    push byte 12
+    push 12
     jmp exc_housekeeping
 
 ; 13: General Protection Fault Exception (With Error Code)
 _exc13:
     cli
-    push byte 13
+    push 13
     jmp exc_housekeeping
 
 ; 14: Page Fault Exception (With Error Code)
 _exc14:
     cli
-    push byte 14
+    push 14
     jmp exc_housekeeping
 
 ; 15: Reserved Exception
 _exc15:
     cli
-    push byte 0
-    push byte 15
+    push 0
+    push 15
     jmp exc_housekeeping
 
 ; 16: Floating Point Exception
 _exc16:
     cli
-    push byte 0
-    push byte 16
+    push 0
+    push 16
     jmp exc_housekeeping
 
 ; 17: Alignment Check Exception
 _exc17:
     cli
-    push byte 0
-    push byte 17
+    push 0
+    push 17
     jmp exc_housekeeping
 
 ; 18: Machine Check Exception
 _exc18:
     cli
-    push byte 0
-    push byte 18
+    push 0
+    push 18
     jmp exc_housekeeping
 
 ; 19: Reserved
 _exc19:
     cli
-    push byte 0
-    push byte 19
+    push 0
+    push 19
     jmp exc_housekeeping
 
 ; 20: Reserved
 _exc20:
     cli
-    push byte 0
-    push byte 20
+    push 0
+    push 20
     jmp exc_housekeeping
 
 ; 21: Reserved
 _exc21:
     cli
-    push byte 0
-    push byte 21
+    push 0
+    push 21
     jmp exc_housekeeping
 
 ; 22: Reserved
 _exc22:
     cli
-    push byte 0
-    push byte 22
+    push 0
+    push 22
     jmp exc_housekeeping
 
 ; 23: Reserved
 _exc23:
     cli
-    push byte 0
-    push byte 23
+    push 0
+    push 23
     jmp exc_housekeeping
 
 ; 24: Reserved
 _exc24:
     cli
-    push byte 0
-    push byte 24
+    push 0
+    push 24
     jmp exc_housekeeping
 
 ; 25: Reserved
 _exc25:
     cli
-    push byte 0
-    push byte 25
+    push 0
+    push 25
     jmp exc_housekeeping
 
 ; 26: Reserved
 _exc26:
     cli
-    push byte 0
-    push byte 26
+    push 0
+    push 26
     jmp exc_housekeeping
 
 ; 27: Reserved
 _exc27:
     cli
-    push byte 0
-    push byte 27
+    push 0
+    push 27
     jmp exc_housekeeping
 
 ; 28: Reserved
 _exc28:
     cli
-    push byte 0
-    push byte 28
+    push 0
+    push 28
     jmp exc_housekeeping
 
 ; 29: Reserved
 _exc29:
     cli
-    push byte 0
-    push byte 29
+    push 0
+    push 29
     jmp exc_housekeeping
 
 ; 30: Reserved
 _exc30:
     cli
-    push byte 0
-    push byte 30
+    push 0
+    push 30
     jmp exc_housekeeping
 
 ; 31: Reserved
 _exc31:
     cli
-    push byte 0
-    push byte 31
+    push 0
+    push 31
     jmp exc_housekeeping
 
 _irq0:
 	cli
-	push byte 0
-	push byte 32
+	push 0
+	push 32
 	jmp irq_housekeeping
 
 _irq1:
 	cli
-	push byte 1
-	push byte 33
+	push 1
+	push 33
 	jmp irq_housekeeping
 
 _irq2:
 	cli
-	push byte 2
-	push byte 34
+	push 2
+	push 34
 	jmp irq_housekeeping
 
 _irq3:
 	cli
-	push byte 3
-	push byte 35
+	push 3
+	push 35
 	jmp irq_housekeeping
 
 _irq4:
 	cli
-	push byte 4
-	push byte 36
+	push 4
+	push 36
 	jmp irq_housekeeping
 
 _irq5:
 	cli
-	push byte 5
-	push byte 37
+	push 5
+	push 37
 	jmp irq_housekeeping
 
 _irq6:
 	cli
-	push byte 6
-	push byte 38
+	push 6
+	push 38
 	jmp irq_housekeeping
 
 _irq7:
 	cli
-	push byte 7
-	push byte 39
+	push 7
+	push 39
 	jmp irq_housekeeping
 
 _irq8:
 	cli
-	push byte 8
-	push byte 40
+	push 8
+	push 40
 	jmp irq_housekeeping
 
 _irq9:
 	cli
-	push byte 9
-	push byte 41
+	push 9
+	push 41
 	jmp irq_housekeeping
 
 _irq10:
 	cli
-	push byte 10
-	push byte 42
+	push 10
+	push 42
 	jmp irq_housekeeping
 
 _irq11:
 	cli
-	push byte 11
-	push byte 43
+	push 11
+	push 43
 	jmp irq_housekeeping
 
 _irq12:
 	cli
-	push byte 12
-	push byte 44
+	push 12
+	push 44
 	jmp irq_housekeeping
 
 _irq13:
 	cli
-	push byte 13
-	push byte 45
+	push 13
+	push 45
 	jmp irq_housekeeping
 
 _irq14:
 	cli
-	push byte 14
-	push byte 46
+	push 14
+	push 46
 	jmp irq_housekeeping
 
 _irq15:
 	cli
-	push byte 15
-	push byte 47
+	push 15
+	push 47
 	jmp irq_housekeeping
